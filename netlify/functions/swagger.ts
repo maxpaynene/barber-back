bb0d2bd30fb3cd5aae7d55145b73da1b2cd2889d
Vilches, Marcelo <marcelo.vilches@cencosud.cl>
2026-02-12 14:10:36 -0300

    fix swagger on netlify

--- netlify/functions/swagger.ts
+++ netlify/functions/swagger.ts
@@ -1,42 +1,36 @@
-// eslint-disable-next-line @typescript-eslint/require-await
-export const handler = async () => {
-  const swaggerHtml = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Barberia API - Swagger UI</title>
-    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui.css">
-</head>
-<body>
-    <div id="swagger-ui"></div>
-    <script src="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-bundle.js"></script>
-    <script src="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-standalone-preset.js"></script>
-    <script>
-    window.onload = function() {
-        SwaggerUIBundle({
-            spec: {
-                openapi: '3.0.0',
-                info: {
-                    title: 'Barberia API',
-                    version: '1.0.0',
-                    description: 'API para gestión de barbería'
-                },
-                servers: [{ url: '/.netlify/functions/api' }]
-            },
-            dom_id: '#swagger-ui',
-            presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-            layout: "StandaloneLayout"
-        });
-    };
-    </script>
-</body>
-</html>
-  `;
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from '../../src/app.module';
+import express from 'express';
+import serverless from 'serverless-http';
 
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'text/html' },
-    body: swaggerHtml,
-  };
+let cachedServer: any;
+
+async function bootstrapServer() {
+  if (!cachedServer) {
+    const expressApp = express();
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+    // Configurar Swagger
+    const config = new DocumentBuilder()
+      .setTitle('Barberia API')
+      .setDescription('API para gestión de barbería')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document);
+
+    await app.init();
+    cachedServer = serverless(expressApp);
+  }
+  return cachedServer;
+}
+
+export const handler = async (event: any, context: any) => {
+  const server = await bootstrapServer();
+  return server(event, context);
 };
