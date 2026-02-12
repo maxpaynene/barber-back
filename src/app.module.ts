a9aef8eac35c82337258d79f342986ec2654f058
Vilches, Marcelo <marcelo.vilches@cencosud.cl>
2026-02-12 14:23:52 -0300

    fix swagger on netlify

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServicesModule } from './services/services.module';
+import { Service } from './services/entities/service.entity';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -19,7 +21,7 @@ import { ServicesModule } from './services/services.module';
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        entities: [Service, User],
         synchronize: false,
         logging: true,
       }),
