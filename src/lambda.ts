750a677520f5e77a366798c86c56d176c85fce21
Vilches, Marcelo <marcelo.vilches@cencosud.cl>
2026-02-12 11:11:29 -0300

    fix up netlify

---
+++ src/lambda.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import serverlessExpress from '@vendia/serverless-express';
+import express from 'express';
+import { Handler, Context, Callback } from 'aws-lambda';
+
+let cachedServer: Handler;
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+    nestApp.enableCors();
+    await nestApp.init();
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    cachedServer = serverlessExpress({ app: expressApp });
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return cachedServer(event, context, callback);
+};
