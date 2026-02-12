92547e16bf48b700997a7d4b0ce709895d957b71
Vilches, Marcelo <marcelo.vilches@cencosud.cl>
2026-02-12 13:36:10 -0300

    fix swagger on netlify

---
+++ netlify/functions/api.ts
@@ -0,0 +1,2 @@
+import { handler } from '../../dist/lambda';
+export { handler };
--- netlify/functions/swagger.ts
+++ netlify/functions/swagger.ts
@@ -1,10 +1,6 @@
-import swaggerUi from 'swagger-ui-express';
-import express from 'express';
-import serverless from 'serverless-http';
+import { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
 
-const app = express();
-
-const swaggerDocument = {
+const swaggerSpec = {
   openapi: '3.0.0',
   info: {
     title: 'Barberia API',
@@ -19,6 +15,52 @@ const swaggerDocument = {
   ],
 };
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerHtml = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Barberia API - Swagger UI</title>
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui.css">
+    <style>
+        html { box-sizing: border-box; overflow: -moz-scrollbars-vertical; overflow-y: scroll; }
+        *, *:before, *:after { box-sizing: inherit; }
+        body { margin:0; padding:0; }
+    </style>
+</head>
+<body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-bundle.js"></script>
+    <script src="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-standalone-preset.js"></script>
+    <script>
+    window.onload = function() {
+        const ui = SwaggerUIBundle({
+            spec: ${JSON.stringify(swaggerSpec)},
+            dom_id: '#swagger-ui',
+            deepLinking: true,
+            presets: [
+                SwaggerUIBundle.presets.apis,
+                SwaggerUIStandalonePreset
+            ],
+            plugins: [
+                SwaggerUIBundle.plugins.DownloadUrl
+            ],
+            layout: "StandaloneLayout"
+        });
+        window.ui = ui;
+    };
+    </script>
+</body>
+</html>
+`;
 
-export const handler = serverless(app);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/require-await
+export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: swaggerHtml,
+  };
+};
