35441d4adfc99865ebf99c987ec3faa4da870499
Vilches, Marcelo <marcelo.vilches@cencosud.cl>
2026-07-13 17:05:12 -0400

    fix netlify

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { setupApp, GLOBAL_PREFIX, SWAGGER_PATH } from './app.setup';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await setupApp(app);
-  await app.listen(3000);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port);
 
-  console.log(`Swagger is running on: ${await app.getUrl()}/${GLOBAL_PREFIX}/${SWAGGER_PATH}`);
-  console.log(`Application is running on: ${await app.getUrl()}/${GLOBAL_PREFIX}/`);
+  console.log(`Swagger UI: ${await app.getUrl()}/${SWAGGER_PATH}`);
+  console.log(`Application: ${await app.getUrl()}/${GLOBAL_PREFIX}/`);
 }
 
 void bootstrap();
